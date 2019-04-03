4562fbfa83df3f10cfbd3d0f465d750135e1af4c
RomanKrylyshyn <rkrylyshyn@gmail.com>
2019-04-03 19:19:56 +0300

    Style errors and messeges

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ def edit
 end
 def update
 	if @article.update(article_params)
-		flash[:notice] ="Article was successfully updated"
+		flash[:success] ="Article was successfully updated"
 		redirect_to article_path(@article)
 	else
 		render 'edit'
@@ -32,7 +32,7 @@ end
 
 def destroy
 	@article.destroy
-	flash[:notice] = "Article was successfully deleted"
+	flash[:danger] = "Article was successfully deleted"
 	redirect_to articles_path
 
 end
@@ -41,7 +41,7 @@ end
 def create
 @article = Article.new(article_params)
 if @article.save
-	flash[:notice] = "Article was successfully created"
+	flash[:success] = "Article was successfully created"
 	redirect_to article_path(@article)
 
 else
