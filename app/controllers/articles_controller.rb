ca53003565c6f4378b2072d92acb0f09b7abf90a
RomanKrylyshyn <rkrylyshyn@gmail.com>
2019-04-03 14:17:29 +0300

    Create new temple

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,30 @@ def new
   @article = Article.new
 end
 
+def show
+ @article = Article.find(params[:id])
+
+end
+
+
+
 def create
-	#render plain: params[:article].inspect
 @article = Article.new(article_params)
-@article.save
-redirect_to article_show(@article)
+if @article.save
+	flash[:notice] = "Article was successfully created"
+	redirect_to article_path(@article)
+
+else
+	render 'new'
+
+end
+
+
+
+
+
+
+
 end
 
 
